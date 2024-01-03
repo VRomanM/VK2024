45d66fbb66fdeaa87442357e9c229600c97421bc
VRM <vromanm@mail.ru>
2024-01-03 19:41:31 +0300

    Перенес связь с делегатом и дата сорсом таблицы новостей в дит сеттер

---
+++ VK2024/Cells/NewsTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTVCell.swift
+//  VK2024
+//
+//  Created by Роман Вертячих on 17.12.2023.
+//
+
+import UIKit
+
+class NewsTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
