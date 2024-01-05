31318580748db8c7409f07499bc562a49e507c7c
VRM <vromanm@mail.ru>
2024-01-05 23:28:55 +0300

    Добавил переход через нав. бар на форму с фотографиями

---
+++ VK2024/Model/ApiVK.swift
@@ -0,0 +1,8 @@
+//
+//  ApiVK.swift
+//  VK2024
+//
+//  Created by Роман Вертячих on 04.01.2024.
+//
+
+import Foundation
---
+++ VK2024/View/PhotoCVCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCVCell.swift
+//  VK2024
+//
+//  Created by Роман Вертячих on 04.01.2024.
+//
+
+import UIKit
+
+class PhotoCVCell: UICollectionViewCell {
+    
+}
