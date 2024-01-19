8bcd60ea03f53778c97bf60208a0b0ef1b0dd685
VRM <vromanm@mail.ru>
2024-01-19 20:45:36 +0300

    Добавил аватар с тенью для FriendTableViewCell

--- VK2024/Controllers/FriendAvatarView.swift
+++ VK2024/Controllers/FriendAvatarView.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class FriendAvatarView: UIView {
+    @IBOutlet var friendAvatar: UIImageView!
+    @IBOutlet var shadowView: UIView!
+    
+    let shadowColor = UIColor.black
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        shadowView.layer.shadowColor = shadowColor.cgColor
+        shadowView.layer.shadowOffset = .zero
+        shadowView.layer.shadowRadius = 7
+        shadowView.layer.shadowOpacity = 0.9
+    }
 
-
+    override func layoutSubviews() {
+        friendAvatar.layer.cornerRadius = bounds.height / 2
+        shadowView.layer.cornerRadius = bounds.height / 2
+    }
 }
